5e60403edbe1ab7d3e054fdfd1d1908ca0556ad5
Otto Vänttinen <kingultimateman@gmail.com>
2021-09-01 03:50:51 +0300

    Add project files.

---
+++ WazoTool/Legacy.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WazoTool
+{
+    /// <summary>
+    /// Interaction logic for Legacy.xaml
+    /// </summary>
+    public partial class Legacy : Window
+    {
+        public Legacy()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMainPage_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow paasivu = new MainWindow();
+
+            paasivu.Show();
+
+            legacyWindow.Hide();
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLegacy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WazoTool/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WazoTool
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Legacy legacy = new Legacy();
+            
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            //Quit out of application
+            Environment.Exit( 1 );
+            //Old way just closes it from tray
+            //Close();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            //Minimizes the application
+            mainWindow.WindowState = WindowState.Minimized;
+            mainWindow.ShowInTaskbar = true;
+        }
+
+        private void btnLegacy_Click(object sender, RoutedEventArgs e)
+        {
+            //Create an object from the legacy tab window
+            //Then show it while hiding mainwindow
+            Legacy uusilegacy = new Legacy();
+            mainWindow.TransformToAncestor(uusilegacy);
+            uusilegacy.Show();
+            mainWindow.Hide();
+            //
+        }
+    }
+}
